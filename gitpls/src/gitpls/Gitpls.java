f1adcc6662f566af4ca260a069925bc485db822f
19527986-1 <19527986-1@SNO_LAB_A30416.inacap.cl>
2016-04-22 14:43:22 -0300

    Romanito

---
+++ gitpls/src/gitpls/Gitpls.java
@@ -0,0 +1,33 @@
+package gitpls;
+
+
+
+public class Gitpls {
+public static void main(String[] args) {
+
+String[] romano={"","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII"};
+int r1=0;
+int r2=0;
+int result=0;
+String I="1";
+String II="2";
+String III="3";
+String IV="4";
+String V="5";
+String VI="6";
+
+r1=Integer.parseInt(VI);
+r2=Integer.parseInt(III);
+result=r1+r2;
+    System.out.println(romano[result]);
+
+
+
+
+
+
+
+
+
+
+}}
